606a6ed35da74b83dd0735c0ece8ade946a44936
Dung Ngo Le Quoc <quocdungngole@gmail.com>
2026-01-18 22:20:27 +0700

    add script to run experiments, pending run

---
+++ run_deterministic_experiments.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# run deterministic experiments
+#
+# each run takes ~25min on my machine, so 1hr in total
+# spec: CPU: AMD Ryzen 9 7900X (24) @ 5.733GHz
+#       Memory: 64GB
+#
+# NOTE: Flag discrepancies between py, cpp, go:
+#   - Beta: py uses --b, cpp/go use --beta
+#   - Output dir: py has none, cpp defaults to data_cpp, go defaults to data_go
+#   - Theta params: py has none, cpp/go have --theta-start, --theta-end, --theta-step
+#   - Deterministic: cpp requires --deterministic=true, py/go use --deterministic flag
+
+go build -o teesside-go main.go
+
+t=$(nproc)
+# save a core so that you can feel safe running this montrosity
+t=$((t - 1))
+
+parallel_params="--halt now,fail=1 -j $t --eta --progress --bar"
+
+pd_params="--game-type pd --beta {1}"
+pgg_params="--game-type pgg --r {1}"
+
+pop_params="--strategy pop --pc {2}"
+neb_params="--strategy neb --nc {2}"
+
+command_params="--deterministic --output-dir data_deterministic_go"
+
+parallel $parallel_params ./teesside-go $command_params $pd_params $pop_params --seed-start {3} --seed-end '$(('{3}'+1))' \
+    ::: 1.2 1.8 2.0 \
+    ::: 0.25 0.5 0.75 0.90 0.92 0.94 0.96 0.98 1.0 \
+    ::: {0..49}
+
+exit
+
+parallel $parallel_params ./teesside-go $command_params $pd_params $neb_params --seed-start {3} --seed-end '$(('{3}'+1))' \
+    ::: 1.2 1.8 2.0 \
+    ::: 1 2 3 4 \
+    ::: {0..49}
+
+parallel $parallel_params ./teesside-go $command_params $pgg_params $pop_params --seed-start {3} --seed-end '$(('{3}'+1))' \
+    ::: 1.5 3 4.5 \
+    ::: 0.25 0.5 0.75 0.90 0.92 0.94 0.96 0.98 1.0 \
+    ::: {0..49}
+
+parallel $parallel_params ./teesside-go $command_params $pgg_params $neb_params --seed-start {3} --seed-end '$(('{3}'+1))' \
+    ::: 1.5 3 4.5 \
+    ::: 1 2 3 4 \
+    ::: {0..49}
