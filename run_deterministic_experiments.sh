35653982ebf65b921eb8a0c358f24eb3dbf0e22e
Dung Ngo <quocdungngole@gmail.com>
2026-01-19 11:27:39 +0700

    update final time result

--- run_deterministic_experiments.sh
+++ run_deterministic_experiments.sh
@@ -11,6 +11,12 @@
 #   - Output dir: py has none, cpp defaults to data_cpp, go defaults to data_go
 #   - Theta params: py has none, cpp/go have --theta-start, --theta-end, --theta-step
 #   - Deterministic: cpp requires --deterministic=true, py/go use --deterministic flag
+#
+# final time result
+#     pd pop = 1350 35m07s
+#     pd neb =  600 15m23s
+#     pd pop = 1350 14m08s
+#     pd pop =  600 6m42s
 
 go build -o teesside-go main.go
 
@@ -32,8 +38,6 @@ parallel $parallel_params ./teesside-go $command_params $pd_params $pop_params -
     ::: 1.2 1.8 2.0 \
     ::: 0.25 0.5 0.75 0.90 0.92 0.94 0.96 0.98 1.0 \
     ::: {0..49}
-
-exit
 
 parallel $parallel_params ./teesside-go $command_params $pd_params $neb_params --seed-start {3} --seed-end '$(('{3}'+1))' \
     ::: 1.2 1.8 2.0 \
